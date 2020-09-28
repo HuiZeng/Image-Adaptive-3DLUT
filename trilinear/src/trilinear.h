15915bcef193c7f62fbc35cc444d1c837f798def
Hui Zeng <HuiZeng@users.noreply.github.com>
2020-09-28 09:26:26 +0800

    Update trilinear.h

--- trilinear/src/trilinear.h
+++ trilinear/src/trilinear.h
@@ -1,5 +1,6 @@
-int trilinear_forward(THCudaTensor * lut, THCudaTensor * image, THCudaTensor * output,
+int trilinear_forward(THFloatTensor * lut, THFloatTensor * image, THFloatTensor * output,
                       int lut_dim, int shift, float binsize, int width, int height, int batch);
 
-int trilinear_backward(THCudaTensor * image, THCudaTensor * image_grad, THCudaTensor * lut_grad,
+int trilinear_backward(THFloatTensor * image, THFloatTensor * image_grad, THFloatTensor * lut_grad,
                        int lut_dim, int shift, float binsize, int width, int height, int batch);
+
